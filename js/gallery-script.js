a214e1a3749b7f8fd2939b06cd23b1a84b46444f
Tay-chat <125014119+Tay-chat@users.noreply.github.com>
2024-01-20 23:39:19 -0500

    Updated gallery

--- js/gallery-script.js
+++ js/gallery-script.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
 		{ src: 'https://i.ibb.co/V26GtZ9/static.png', title: 'Static', desc: 'This guy again?' },
         { src: 'https://i.ibb.co/GJDDV4x/vision.png', title: 'Vision', desc: 'See no evil' },
         { src: 'https://i.ibb.co/DW9bHpM/Entity.png', title: 'Entity', desc: 'So this is what was in the dark corner' },
-        { src: 'https://i.ibb.co/JshQpVr/Heat.png', title: 'Heat', desc: 'Bet your bottom dollar' },		
+        { src: 'https://i.ibb.co/Q6b2Zxp/Heat.png', title: 'Heat', desc: 'Bet your bottom dollar' },		
 		
         // Add more images as needed
     ];
